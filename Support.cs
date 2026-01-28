e2299fe24b6009c41abb4d78c28e43778780bac3
Kajzawesome <kaj.jacobs@gmail.com>
2026-01-28 09:20:57 -0700

    added print board

--- Support.cs
+++ Support.cs
@@ -5,4 +5,55 @@ public class Support
 	public Support()
 	{
 	}
+
+	public void printBoard(char[] board)
+	{
+		string strBoard = "";
+        for (int i = 0; i < 9; i += 3)
+		{
+            // space for first cell
+            strBoard += " ";
+			if (board[i] == ' ')
+			{
+				strBoard += (i + 1).ToString();
+            }
+            else
+            {
+               strBoard += board[i];
+            }
+
+            strBoard += " | ";
+            // space for second cell
+            if (board[i+1] == ' ')
+            {
+                strBoard += (i + 2).ToString();
+            }
+            else
+            {
+                strBoard += board[i+1];
+            }
+
+            strBoard += " | ";
+            // space for third cell
+            if (board[i+2] == ' ')
+            {
+                strBoard += (i + 3).ToString();
+            }
+            else
+            {
+                strBoard += board[i+2];
+            }
+
+            if (i < 6)
+			{
+				strBoard += "\n---+---+---\n";
+			}
+        }
+        Console.WriteLine(strBoard);
+    }
+
+	public bool gameOver(char[] board)
+	{
+
+	}
 }
